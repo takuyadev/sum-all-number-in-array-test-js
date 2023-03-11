22e4fdf41588aec92ee43648d2a01c2e207c54fb
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-10 16:23:52 -0800

    i... finished...

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+
+  // If array is more than one, keep going
+  // Recursive case
+  if (array.length > 1) {
+
+    // Check if current iteration is an array
+    if (Array.isArray(array[array.length - 1])) {
+
+      // YOU HAVE TO RETURN IT TO THE ARRAY??
+      array[array.length - 1] = sumItems(array[array.length - 1]);
+    }
+
+    // Pop the end of the array and add it to the first value
+    array[0] += array.pop();
+    return sumItems(array);
+  }
+  
+  return array[0];
 }
 
-module.exports = sumItems;
+console.log(sumItems([1, 2, [2, 2, 2], 3]));
